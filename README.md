1e53af54cb6c1851d4d48b2185f980de51d80ba6
Omid <omidmogasemi@gmail.com>
2022-06-02 07:37:36 -0700

    Add template, more test images, display classification in HTML, finalize README.md

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Setup 
+# Introduction 
+Welcome to the repository for Group 5's ECS 171 assignment. This repository contains a Flask server capable of running and demonstrating our model. 
+
+To use it, simply follow the Setup instructions below. Once your application is running, you can upload any image of a skin lesion to have it classified. There are 5 sample images for your use located in the `test_images` folder. Any images you upload will subsequently be placed into the `uploaded_images` folder for Flask's processing. 
+
+## Setup 
 1. To install the necessary packages uses in `server.py`, run the following command in the root directory of this project: `conda create --name <env> --file requirements.txt`, where `<env>` is your desired name of the environment. 
 2. Run `conda activate <env>` to activate this environment. 
 3. Run `export FLASK_APP=server` to set the environment variable `FLASK_APP` to `server.py`. 
-3. Run `flask run` to start the server. 
-
-I will add a more concise description later of how our application works replacing the onboarding screen from our previous application as well as describing how to use the actual program itself. 
+4. Run `flask run` to start the server. 
+5. Navigate to `http://127.0.0.1:5000/` in your browser. 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, render_template, request 
 from werkzeug.utils import secure_filename
 from PIL import Image
 import torch 
@@ -11,38 +11,32 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Helper function from Flask documentation to determine if an uploaded filename 
+# is an ALLOWED_EXTENSIONS file type 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    res = "Not calculated" # Return no calculation if no image was properly uploadedm 
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+            return render_template('index.html', res=res) 
+        
+        file = request.files['file'] # extract file from request 
+        # If no file submitted (and thus a default empty string) 
         if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            # TODO -> Capture the output and display it nicely for the redirect on line 34. 
-            # TODO -> Also display the classification. You will need to call .argmax() on the out variable in the eval function. 
-            print(eval(Image.open(file))) 
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('download_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            return render_template('index.html', res=res) 
+        
+        # Properly uploaded file 
+        if file and allowed_file(file.filename): 
+            res = eval(Image.open(file)) 
+    
+    return render_template('index.html', res=res) 
 
+# Classify the image using the downloaded model 
 def eval(image): 
     # Load data transformer and model file using CPU only 
     data_transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()]) 
@@ -50,4 +44,7 @@ def eval(image):
     model.eval() 
     image = data_transform(image).unsqueeze(0) # Transform the image 
     out = model(image) # Run the image against the model 
-    return out 
+    
+    # Assign the class label with the highest probability argument 
+    labels = ["akiec (Actinic Keratoses and Bowen’s Disease)", "bcc (Basal Cell Carcinoma)", "bkl (Benign Keratosis-like Lesions)", "df (Dermatofibroma)", "mel (Melanoma)", "nv (Melanocytic nevi)", "vasc (Vascular Lesions)"] 
+    return labels[out.argmax()] 
